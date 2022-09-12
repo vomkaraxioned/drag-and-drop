8255c908dea1cc69856f98edb400f3c9e2d4cbbc
vomkaraxioned <omkarv@axioned.com>
2022-09-12 11:07:33 +0530

    js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,44 @@
 /* Author: 
 
 */
-const start =document.querySelector(".start");
-const restart =document.querySelector(".restart");
+const start = document.querySelector(".start");
+const restart = document.querySelector(".restart");
 const priorities = document.querySelectorAll(".drop-area");
 const tasks = document.querySelectorAll(".task");
-let dragged, stop = false ;
+let dragged, stop = false;
 
 function setDraggable(value) {
-    tasks.forEach((task)=>{
+    tasks.forEach((task) => {
         task.draggable = value;
-         task.addEventListener('dragstart',(e)=>{
+        task.addEventListener('dragstart', (e) => {
             dragged = e.target;
-         });
-     }
-     );
+        });
+    });
 }
 
-restart.addEventListener('click',(e)=>{
+restart.addEventListener('click', (e) => {
     location.reload();
 });
 
-start.addEventListener('click',()=>{
-    if(stop){
+start.addEventListener('click', () => {
+    if (stop) {
         start.innerHTML = "start";
         setDraggable(false);
-        stop =false ;
-    }else{
+        stop = false;
+    } else {
         start.innerHTML = "stop";
-       setDraggable(true);
+        setDraggable(true);
+        stop = true;
     }
-    stop =true;
 });
 
 
-priorities.forEach((priority)=>{
-    priority.addEventListener('dragover',(e)=>{
+priorities.forEach((priority) => {
+    priority.addEventListener('dragover', (e) => {
         e.preventDefault();
     });
-   priority.addEventListener('drop',(e)=>{
-   priority.appendChild(dragged);
-    e.preventDefault();
-   });
+    priority.addEventListener('drop', (e) => {
+        priority.appendChild(dragged);
+        e.preventDefault();
+    });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
